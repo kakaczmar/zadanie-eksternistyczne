353c464b760fd969a7fb6ee9e8102463ae5a9a81
Rafał Adach <rafal.adach95@gmail.com>
2022-08-28 14:38:09 +0200

    Obliczanie dla 1 pliku

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,14 @@
-public class App {
+import org.apache.poi.ss.usermodel.Workbook;
 
+public class App {
     public static void main(String[] args) {
         System.out.println("main");
-    }
+        String file = "src/main/resources/2012/01/Kowalski_Jan.xls";
 
+        ExcelLoader excelLoader = new ExcelLoader();
+        Calculator calculator = new Calculator();
+        Workbook wb = excelLoader.loadExcelFile(file);
+
+        System.out.println(calculator.calculateHoursWorked(wb));
+    }
 }
---
+++ src/main/java/Calculator.java
@@ -0,0 +1,21 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+public class Calculator {
+
+    public int calculateHoursWorked(Workbook workbook) {
+        int hoursWorked =0;
+        for (Sheet sheet: workbook) {
+            System.out.println(sheet.getSheetName());
+            for (Row row: sheet) {
+                if (row.getRowNum()!=0) {
+                    hoursWorked += row.getCell(2).getNumericCellValue();
+                }
+            }
+        }
+        return hoursWorked;
+
+    }
+
+}
